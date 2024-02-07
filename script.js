9b0a1d926a699e49b1e289ed562346f36b7ca685
Luan <luam10@hotmail.com.br>
2024-02-06 23:03:23 -0300

    git init

---
+++ script.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form')
+const ContatoNome = []
+const numeroContato = []
+
+console.log(form)
+
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+
+
+
+})
