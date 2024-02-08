a615dcdcc8ce4b3a9d5a16d76408e3f8239a9364
Luan <luam10@hotmail.com.br>
2024-02-07 22:26:49 -0300

    criando logica

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 const form = document.querySelector('form')
-const ContatoNome = []
+const contatoNome = []
 const numeroContato = []
 
-console.log(form)
+let linhas= ''
+
 
 form.addEventListener('submit',function(e){
     e.preventDefault()
 
-
-
-
+    addLinha()
+    criaTabela()
 })
+
+function addLinha(){
+    const nome = document.getElementById('nome')
+    const fone = document.getElementById('fone')
+
+    if(contatoNome.includes(nome.value)){
+        alert('ja tem')
+    }else if(fone.length <= 9){
+        Alert('Coloque o telefone Valido')
+
+    }else{
+        contatoNome.push(nome.value)
+
+        let linha = `<tr> `
+        linha += `<td> ${nome.value} </td>`
+        linha += `<td> ${fone.value} </td>` 
+        linha += `</tr>`
+    
+        linhas += linha
+    }
+
+    nome.value = ''
+    fone.value = ''
+}
+
+
+
+function criaTabela(){
+    const tbody = document.querySelector('tbody')
+    tbody.innerHTML = linhas
+}
