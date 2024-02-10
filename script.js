e20347594f5e63085c867b3f8181a1d0274add2c
Luan <luam10@hotmail.com.br>
2024-02-09 21:05:32 -0300

    criando resposta invalida

--- script.js
+++ script.js
@@ -19,32 +19,39 @@ function addLinha(){
 
     //Verifica se ja existe um nome de contato cadastrado e input esta valido
     if(contatoNome.includes(nome.value) || nome.value === null){
-        alert('ja tem')
+        alert('Contato ja cadastrado')
 
     // Verifica se o telefone tem o tamanho correto    
-    }else if(fone.length > 9 || fone.length < 8){
+    }else if(fone.value.length !== 9){
         alert('Coloque o telefone Valido')
+        fone.value = ''
+
 
     // Verificar se o numero de contato ja existe
     }else if(numeroContato.includes(fone.value)){
-        alert('Numero ja add')
+        alert('Numero ja cadastrado')
 
     // add as linhas     
     } else{
         contatoNome.push(nome.value)
 
-        let linha = `<tr> `
-        linha += `<td> ${nome.value} </td>`
-        linha += `<td> ${fone.value} </td>` 
-        linha += `</tr>`
-    
-        linhas += linha
+        criaLinha()
     }
 
     nome.value = ''
     fone.value = ''
 }
 
+
+
+function criaLinha(){
+    let linha = `<tr> `
+    linha += `<td> ${nome.value} </td>`
+    linha += `<td> ${fone.value} </td>` 
+    linha += `</tr>`
+
+    linhas += linha
+}
 
 function criaTabela(){
     const tbody = document.querySelector('tbody')
