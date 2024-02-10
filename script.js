888863a1aa49756bd8296eda5e3d2d215694ba06
Luan <luam10@hotmail.com.br>
2024-02-09 22:04:21 -0300

    inclusão de msgs

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
 const form = document.querySelector('form')
 const contatoNome = []
 const numeroContato = []
-
+const nome = document.getElementById('nome')
+const fone = document.getElementById('fone')
 let linhas= ''
 
 
 form.addEventListener('submit',function(e){
     e.preventDefault()
 
-    addLinha()
+    Verificar()
     criaTabela()
+
 })
 
 
-function addLinha(){
-    const nome = document.getElementById('nome')
-    const fone = document.getElementById('fone')
+function Verificar(){
+    
+    const p = document.querySelector('p')[0]
+    const nomeExistente = `Contato ja cadastrado`
+    const foneInvalido = `Digite um telefone valido`
+    const foneExistente = `Numero ja cadastrado`
 
+
+    
     //Verifica se ja existe um nome de contato cadastrado e input esta valido
     if(contatoNome.includes(nome.value) || nome.value === null){
+        nome.classList.add('inputInvalido')
         alert('Contato ja cadastrado')
+
+
 
     // Verifica se o telefone tem o tamanho correto    
     }else if(fone.value.length !== 9){
+        fone.classList.add('inputInvalido')
         alert('Coloque o telefone Valido')
-        fone.value = ''
+
 
 
     // Verificar se o numero de contato ja existe
     }else if(numeroContato.includes(fone.value)){
+        fone.classList.add('inputInvalido')
         alert('Numero ja cadastrado')
+
 
     // add as linhas     
     } else{
+        numeroContato.push(fone.value)
         contatoNome.push(nome.value)
+        fone.classList.remove('inputInvalido')
+        nome.classList.remove ('inputInvalido')
 
         criaLinha()
     }
@@ -56,4 +72,13 @@ function criaLinha(){
 function criaTabela(){
     const tbody = document.querySelector('tbody')
     tbody.innerHTML = linhas
+}
+
+
+function Msg(){
+    nome.classList('inputInvalido')
+}
+
+function totalContato(){
+
 }
