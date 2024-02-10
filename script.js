a1cd3c6c3b085cb785972a4762aaff9140c633c0
Luan <luam10@hotmail.com.br>
2024-02-09 23:06:23 -0300

    criando total

--- script.js
+++ script.js
@@ -11,54 +11,47 @@ form.addEventListener('submit',function(e){
 
     Verificar()
     criaTabela()
-
+    totalContato()
 })
 
 
 function Verificar(){
     
-    const p = document.querySelector('p')[0]
+    const p = document.querySelector('p')
     const nomeExistente = `Contato ja cadastrado`
     const foneInvalido = `Digite um telefone valido`
     const foneExistente = `Numero ja cadastrado`
 
-
-    
     //Verifica se ja existe um nome de contato cadastrado e input esta valido
-    if(contatoNome.includes(nome.value) || nome.value === null){
+    if(contatoNome.includes(nome.value)){
         nome.classList.add('inputInvalido')
-        alert('Contato ja cadastrado')
-
+        p.innerHTML = nomeExistente
 
 
     // Verifica se o telefone tem o tamanho correto    
     }else if(fone.value.length !== 9){
         fone.classList.add('inputInvalido')
-        alert('Coloque o telefone Valido')
-
+        p.innerHTML = foneInvalido
 
 
     // Verificar se o numero de contato ja existe
     }else if(numeroContato.includes(fone.value)){
         fone.classList.add('inputInvalido')
-        alert('Numero ja cadastrado')
+        p.innerHTML = foneExistente
 
 
     // add as linhas     
     } else{
         numeroContato.push(fone.value)
         contatoNome.push(nome.value)
-        fone.classList.remove('inputInvalido')
-        nome.classList.remove ('inputInvalido')
 
         criaLinha()
     }
-
     nome.value = ''
     fone.value = ''
+    fone.classList.remove('inputInvalido')
+    nome.classList.remove ('inputInvalido')
 }
-
-
 
 function criaLinha(){
     let linha = `<tr> `
@@ -74,11 +67,14 @@ function criaTabela(){
     tbody.innerHTML = linhas
 }
 
-
-function Msg(){
-    nome.classList('inputInvalido')
-}
-
 function totalContato(){
 
+    const total = document.getElementById('totalContato')
+    let soma = 1
+    for (let i = 0; i < contatoNome.length; i++) {
+        soma += i
+        
+    }
+
+    total.innerHTML = soma
 }
